c8b235bcfb7a4767b8cc7a49a39aeeae70444c85
jhlee <wisdom_13_@naver.com>
2023-01-03 01:16:49 +0900

    로그인 - 로그아웃 버튼

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
-import { login } from '../api/firebase';
+import { login, logout, onUserStateChange } from '../api/firebase';
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    })
+  })
+
+  const handleLogin = () => {
+    login().then(setUser);
+  }
+  const handleLogout = () => {
+    logout().then(setUser);
+  }
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
       <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -17,7 +32,8 @@ export default function Navbar() {
         <Link to='/products/new'>
           <BsFillPencilFill />
         </Link>
-        <button onClick={login}>Login</button>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
