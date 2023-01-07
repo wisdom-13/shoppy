9aeca1514c535c651170e05e7af16f78157dcb48
jhlee <wisdom_13_@naver.com>
2023-01-07 22:52:22 +0900

    어드민 사용자 - UI 완성

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default function Button({ text, onClick }) {
+  return <button className='bg-brand py-2 px-4 text-white rounded-md hover:brightness-110' onClick={onClick}>{text}</button>
+};
