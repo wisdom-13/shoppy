401aa54421a7af89e4155f93c003ca54a3dacfb5
jhlee <wisdom_13_@naver.com>
2023-01-08 23:07:28 +0900

    어드민 사용자 - 경로 보호

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
-import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
 import Button from './ui/Button';
+import { useAuthContext } from './context/AuthContext';
 
 export default function Navbar() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    onUserStateChange((user) => {
-      console.log(user);
-      setUser(user)
-    })
-  }, [])
+  const { user, login, logout } = useAuthContext();
 
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
@@ -24,7 +17,7 @@ export default function Navbar() {
       </Link>
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
-        <Link to='/carts'>Carts</Link>
+        {user && <Link to='/carts'>Carts</Link>}
         {user && user.isAdmin && (
           <Link to='/products/new'>
             <BsFillPencilFill />
