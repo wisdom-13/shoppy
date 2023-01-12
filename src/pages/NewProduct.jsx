b724c35d14414ca9aa9d8890a5a0e3164715061b
jhlee <wisdom_13_@naver.com>
2023-01-12 13:57:26 +0900

    세로운 제품 등록 - UI 완성

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -6,6 +6,8 @@ import Button from '../components/ui/Button';
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
+  const [isUploading, setIsUploading] = useState(false);
+  const [success, setSuccess] = useState();
 
   const handelChange = (e) => {
     const { name, value, files } = e.target;
@@ -18,22 +20,33 @@ export default function NewProduct() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsUploading(true);
+
     uploadImage(file)
       .then((url) => {
-        addNewProduct(product, url);
+        addNewProduct(product, url)
+          .then(() => {
+            setSuccess('성공적으로 제품이 추가되었습니다.');
+            setTimeout(() => {
+              setSuccess(null);
+            }, 4000)
+          });
       })
+      .finally(() => setIsUploading(false));
   }
   return (
-    <section>
-      {file && <img src={URL.createObjectURL(file)} alt='local file'></img>}
-      <form onSubmit={handleSubmit}>
+    <section className='w-full text-center'>
+      <h2 className='text-2xl font-bold my-4'>새로운 제품 등록</h2>
+      {success && <p className='my-2'>✅ {success}</p>}
+      {file && <img className='w-96 mx-auto mb-2' src={URL.createObjectURL(file)} alt='local file'></img>}
+      <form className='flex flex-col px-12' onSubmit={handleSubmit}>
         <input type='file' accept='image/*' name='file' required onChange={handelChange}></input>
         <input type='text' name='title' value={product.title ?? ''} placeholder='제품명' required onChange={handelChange}></input>
         <input type='number' name='price' value={product.price ?? ''} placeholder='가격' required onChange={handelChange}></input>
         <input type='text' name='category' value={product.category ?? ''} placeholder='카테고리' required onChange={handelChange}></input>
         <input type='text' name='description' value={product.description ?? ''} placeholder='제품 설명' required onChange={handelChange}></input>
         <input type='text' name='options' value={product.options ?? ''} placeholder='옵션 (, 로 구분)' required onChange={handelChange}></input>
-        <Button text={'제품 등록하기'}></Button>
+        <Button text={isUploading ? '업로드중...' : '제품 등록하기'} disabled={isUploading}></Button>
       </form>
     </section>
   );
