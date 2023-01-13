130c0671109ae4c1b5c8657c21c35e18a751ff9c
jhlee <wisdom_13_@naver.com>
2023-01-14 03:30:19 +0900

    제품들 보여주기 - UI

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function ProductCard({ product: { id, image, title, category, price } }) {
 
   return (
-    <li>
-      <img src={image} alt={title}></img>
-      <div>
-        <h3>{title}</h3>
+    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+      <img className='w-full' src={image} alt={title}></img>
+      <div className='mt-2 px-3 text-lg flex justify-between items-center'>
+        <h3 className='truncate'>{title}</h3>
         <p>{`₩${price}`}</p>
       </div>
-      <p>{category}</p>
+      <p className='mb-2 px-3 text-gray-600'>{category}</p>
     </li>
   );
 }
