c209ad2b20383e51c34c381b381dc2c25d3e2688
jhlee <wisdom_13_@naver.com>
2023-01-16 14:17:08 +0900

    제품 상세 페이지 - 데이터 보여주기

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function ProductCard({ product: { id, image, title, category, price } }) {
+export default function ProductCard({ product, product: { id, image, title, category, price } }) {
+
+  const navigate = useNavigate();
 
   return (
-    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+    <li onClick={() => navigate(`/products/${id}`, { state: { product } })} className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'>
       <img className='w-full' src={image} alt={title}></img>
       <div className='mt-2 px-3 text-lg flex justify-between items-center'>
         <h3 className='truncate'>{title}</h3>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
 export default function ProductDetail() {
-  return <div>Product Details</div>;
+  const { state: { product: { id, image, title, description, category, price, options } } } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  }
+  const handleClick = (e) => {
+
+  };
+  return (
+    <section>
+      <p>{category}</p>
+      <img src={image} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>₩{price}</p>
+        <p>{description}</p>
+        <p>옵션:</p>
+        <select onChange={handleSelect} value={selected}>
+          {options && options.map((option, index) => (
+            <option key={index}>{option}</option>
+          ))}
+        </select>
+        <Button text="장바구니에 추가" onClick={handleClick}></Button>
+      </div>
+    </section>
+  );
 }
